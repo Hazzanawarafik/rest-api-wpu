83f1742bf25e263c882ecc084014f78ce9075e1a
Hazzanawarafik <hazzanawa@gmail.com>
2023-12-02 14:06:20 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,19 @@
+<?php 
+// $mahasiswa = [
+//     [
+//     "nama" => "Hazza Nawarafi",
+//     "nrp" => "213412312",
+//     "email" => "hazzanawa@gmail.com"
+//     ],
+//     [
+//     "nama" => "Hazza Nawarafi",
+//     "nrp" => "213412312",
+//     "email" => "hazzanawa@gmail.com"
+//     ]
+// ];
+
+
+// var_dump($mahasiswa);
+$data = json_encode($mahasiswa);
+echo $data;
+?>
